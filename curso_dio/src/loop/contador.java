72ab92e7df38d57d923bb87297f46e4a6235601c
Cristiano Vieira <cpv.cristiano@gmail.com>
2023-05-06 20:54:49 -0300

    mairop numero

---
+++ curso_dio/src/loop/contador.java
@@ -0,0 +1,28 @@
+package loop;
+
+import java.util.Scanner;
+
+/*
+ Faça um programa que leia 5 números e informe o maior número e a média desses números.
+ */
+public class contador {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+
+        int numero;
+        int maior = 0;
+
+        int count = 0;
+        do {
+            System.out.print("Número : ");
+            numero = scan.nextInt();
+
+            if (numero > maior)
+                maior = numero;
+
+            count = count + 1;
+        } while (count < 5);
+
+        System.out.print("O Maior número : " + maior);
+    }
+}
