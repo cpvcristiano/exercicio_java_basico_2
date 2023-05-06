4ea34a79a5fdb74280f56c62765878e442c89867
Cristiano Vieira <cpv.cristiano@gmail.com>
2023-05-06 20:55:16 -0300

    tetando esse exercicio

---
+++ curso_dio/src/loop/MaiorMedia.java
@@ -0,0 +1,29 @@
+package loop;
+
+import java.util.Scanner;
+
+public class MaiorMedia {
+
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+
+        int nota1;
+        int nota2;
+        int nota3;
+        int nota4;
+        int nota5;
+
+        System.out.print("DIGITE A PRIMEIRA NOTA : ");
+        nota1 = scan.nextInt();
+        System.out.print("DIGITE A SEGUNDA NOTA : ");
+        nota1 = scan.nextInt();
+        System.out.print("DIGITE A TERCEIRA NOTA : ");
+        nota1 = scan.nextInt();
+        System.out.print("DIGITE A QUARTA  NOTA : ");
+        nota1 = scan.nextInt();
+        System.out.print("DIGITE A QUINTA NOTA : ");
+        nota1 = scan.nextInt();
+
+    }
+
+}
